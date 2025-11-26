47b4f7d72e84153c51df6b62348840b651bab2f3
Aheed56 <maheed9291@gmail.com>
2025-11-27 00:20:02 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            break;
+        }
+        printf("%d", i);
+    }
+
+    for (i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            continue;
+        }
+    }
+
+    printf("%d", i);
+
+    return 0;
+}
