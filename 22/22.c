6a7e32d56efbe35442d93c543162d6502316a262
Aheed56 <maheed9291@gmail.com>
2025-11-26 23:47:56 +0530

    Update 22.c

--- 22/22.c
+++ 22/22.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
+
 int main()
 {
-	int i,n;
-	long long int x;
-	x=2;
-	
-	printf("Enter the value of n");
-	scanf("%d", &n);
-	
-	for(i=2;i<=n;i++)
-	{
-		x=x*x;
-		printf("%d ",x);
-		
-	}
-	
-		
-	
-	
+    int i, n;
+    long long int x = 2;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        printf("%lld ", x);
+        x = x * x;
+    }
+
+    return 0;
 }
+
